0f610db6bc5c5b21d7d49467bf39e70545e37c58
marcela <marceladominguez1993@gmail.com>
2021-03-18 20:52:22 -0500

    add: add, edit and update a user

---
+++ resources/js/components/users/ListComponent.vue
@@ -0,0 +1,49 @@
+<template>
+</template>
+<script>
+    export default {
+        name: "ListComponent",
+        methods: {
+            createUser: function () {
+                this.users.push({
+                    description: this.new_user,
+                    pending: true,
+                    editing: false,
+                });
+                this.new_user = '';
+            },
+            toggleStatus: function (user) {
+                user.pending = !user.pending;
+            },
+            editUser: function (user) {
+                // permitir que solo se edite una sola vez
+                this.users.forEach( function (user) {
+                    user.editing = false;
+                });
+                user.editing = true;
+            },
+        },
+        data: function() {
+            return  {
+                new_user: '',
+                users: [
+                    {
+                        description: 'Usuario A',
+                        pending: true,
+                        editing: false,
+                    },
+                    {
+                        description: 'Usuario B',
+                        pending: true,
+                        editing: true,
+                    },
+                    {
+                        description: 'Usuario C',
+                        pending: false,
+                        editing: false,
+                    }
+                ]
+            }
+        },
+    }
+</script>
--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -6,80 +6,64 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Usuarios') }}</div>
-
+                    <list-component inline-template>
                     <div class="card-body">
                         <div id="app" class="container">
 
                             <ul class="list-group tasks">
-                                <li class="list-group-item">
-                                    <a href="#">
-                                        <span class="glyphicon glyphicon-unchecked"
+                                <li
+                                    v-for="user in users"
+                                    class="list-group-item"
+                                    :class="{editing: user.editing, completed: !user.pending}">
+                                    <a @click="toggleStatus(user)" href="#">
+                                        <span class="glyphicon"
+                                            :class="user.pending ? 'glyphicon-unchecked' : 'glyphicon-check'"
                                               aria-hidden="true">
                                         </span>
                                     </a>
-                                    <span class="description">Medicamento A</span>
-                                    <div>
-                                        <a href="#">
+
+                                    <template v-if="!user.editing">
+                                        <span class="description">@{{user.description}}</span>
+                                        <div>
+                                            <a @click="editUser(user)">
                                             <span class="glyphicon glyphicon-edit"
                                                   aria-hidden="true">
                                             </span>
-                                        </a>
-                                        <a href="#">
+                                            </a>
+                                            <a href="#">
                                             <span class="glyphicon glyphicon-trash"
                                                   aria-hidden="true">
                                             </span>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li class="list-group-item editing">
-                                    <a href="#">
-                                        <span
-                                            class="glyphicon glyphicon-unchecked"
-                                            aria-hidden="true">
-                                        </span>
-                                    </a>
-                                    <input type="text" value="Medicamento B">
-                                    <div>
-                                        <a href="#">
+                                            </a>
+                                        </div>
+                                    </template>
+
+                                    <template v-else>
+                                        <input type="text" v-model="user.description">
+
+                                        <div>
+                                            <a href="#">
                                             <span class="glyphicon glyphicon-ok"
                                                   aria-hidden="true">
                                             </span>
-                                        </a>
-                                        <a href="#">
+                                            </a>
+                                            <a href="#">
                                             <span class="glyphicon glyphicon-remove"
                                                   aria-hidden="true">
                                             </span>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li class="list-group-item completed">
-                                    <a href="#">
-                                        <span class="glyphicon glyphicon-check"
-                                              aria-hidden="true">
-                                        </span>
-                                    </a>
-                                    <span class="description">Medicamento C</span>
+                                            </a>
+                                        </div>
+                                    </template>
 
-                                    <div>
-                                        <a href="#">
-                                            <span class="glyphicon glyphicon-edit"
-                                                  aria-hidden="true">
-                                            </span>
-                                        </a>
-                                        <a href="#">
-                                            <span class="glyphicon glyphicon-trash"
-                                                  aria-hidden="true">
-                                            </span>
-                                        </a>
-                                    </div>
                                 </li>
                             </ul>
 
-                            <form class="new-task-form">
-                                <input type="text" class="form-control">
+                            <form @submit.prevent="createUser" class="new-task-form">
+                                <input v-model="new_user" type="text" class="form-control">
                                 <button class="btn btn-primary">Crear tarea</button>
                             </form>
                     </div>
+                        </list-component>
                 </div>
             </div>
         </div>
